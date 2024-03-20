b2e2fc4f18a7a3b62509b609ae93dfac84d1b6e1
student.inginerie <student.inginerie@ING-PC-E002-08>
2024-03-20 11:44:24 +0200

    lab3 OpenGL

--- Patrat.cpp
+++ Patrat.cpp
@@ -19,7 +19,8 @@ void CALLBACK MutaJos(void);
 
 static GLfloat x=0;
 static GLfloat y = 0;
-
+static GLfloat alfa = 0;
+static GLUquadricObj* o;
 
 void myinit (void) {
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -43,25 +44,74 @@ void CALLBACK MutaDreapta(void)
 {
 	x=x+10;
 }
+void CALLBACK RotateLeft(AUX_EVENTREC* event)
+{
+    alfa += 15;
+}
+void CALLBACK RotateRight(AUX_EVENTREC *event)
+{
+    alfa -= 15;
+}
 
-float xPos = 0; float yPos = 0; float radius = 60.0f;
+
 void CALLBACK display (void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    
     glLoadIdentity ();
+    o = gluNewQuadric();
+    glRotatef(alfa, 1, 0,0 );
 
-	glTranslatef(x, y, 0.0);
+    glPushMatrix();  
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluCylinder(o, 50.0, 50.0, 100,200,20);
+    glPopMatrix();
 
+    glPushMatrix();
+    glTranslatef(x-100, y, 0.0);
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluSphere(o, 50.0, 100, 100);
+    glPopMatrix();
 
-   
+    glPushMatrix();
+    glTranslatef(x+100, y, 0.0);
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluCylinder(o, 0.0, 50.0, 100, 200, 20);
+    glPopMatrix();
 
+    glPushMatrix();
+    glTranslatef(x + 200, y, 0.0);
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluDisk(o, 20.0, 50.0, 100, 200);
+    glPopMatrix();
 
-        
-    
+    glPushMatrix();
+    glTranslatef(x-200, y, 0.0);
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluDisk(o, 0.0, 50.0, 100, 200);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(x+300, y, 0.0);
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluPartialDisk(o, 20.0, 50.0, 100,20, 0.0,90.0);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(x - 300, y, 0.0);
+    glColor3f(1.0, 0, 0);
+    gluQuadricDrawStyle(o, GLU_FILL);
+    gluPartialDisk(o, 0.0, 50.0, 100, 20, 0.0, 90.0);
+    glPopMatrix();
 
     glFlush();
 }
+
 
 /*void CALLBACK myReshape(GLsizei w, GLsizei h)
 {
@@ -81,11 +131,11 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     if (w <= h)		
-    glOrtho (-160.0, 160.0, 160.0*(GLfloat)h/(GLfloat)w, 
-        -160.0*(GLfloat)h/(GLfloat)w, -10.0, 10.0);
+    glOrtho (-500.0, 500.0, 500.0*(GLfloat)h/(GLfloat)w, 
+        -500.0*(GLfloat)h/(GLfloat)w, -160.0, 160.0);
     else 
-    glOrtho (-160.0*(GLfloat)w/(GLfloat)h, 
-        160.0*(GLfloat)w/(GLfloat)h, -160.0, 160.0, -10.0, 10.0);
+    glOrtho (-500.0*(GLfloat)w/(GLfloat)h,
+        500.0*(GLfloat)w/(GLfloat)h, 500.0, 500.0, -160.0, 160.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -99,7 +149,8 @@ int main(int argc, char** argv)
 	auxKeyFunc (AUX_RIGHT, MutaDreapta);
     auxKeyFunc(AUX_UP, MutaSus);
     auxKeyFunc(AUX_DOWN, MutaJos);
-	
+    auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, RotateLeft);
+    auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, RotateLeft);
     auxReshapeFunc (myReshape);
     auxMainLoop(display);
     return(0);
